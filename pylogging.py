88ea2056fbfa42a453ed32da53f6f35506991a81
rhino007US <rhino007_us@yahoo.com>
2021-07-24 15:07:13 -0500

    chc chc changes

--- pylogging.py
+++ pylogging.py
@@ -1,8 +1,30 @@
 # python's Logging
 import logging
-logging.basicConfig(level=logging.DEBUG, format='asctime)s - %(name)s - %(levelname)s - %(message)s', datefmt='%m/%d/%Y %H:%M:%S')
-logging.debug('this is a debug message')
-logging.info('this is a info message')
-logging.warning('this is a warning message')
-logging.error('this is a error message')
-logging.critical('this is a critical message')
+# logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', datefmt='%m/%d/%Y %H:%M:%S')
+# logging.debug('this is a debug message')
+# logging.info('this is a info message')
+# logging.warning('this is a warning message')
+# logging.error('this is a error message')
+# logging.critical('this is a critical message')
+# import helper
+# stream and file logging, with levels and handlers
+logger = logging.getLogger(__name__)
+
+# create handler
+stream_h = logging.StreamHandler()
+file_h = logging.FileHandler('file.out')
+
+# level and the format
+stream_h.setLevel(logging.WARNING)
+flile_h.setLevel(logging.ERROR)
+
+formatter = logging.Formatter('%(name)s - %(levelname)s - %(message)s')
+stream_h.setFormatter(formatter)
+file_h.setFormatter(formatter)
+
+logger.addHandler(stream_h)
+logger.addHandler(file_h)
+
+logger.warning('this is a warning')
+logger.error('thei is an error')
+
