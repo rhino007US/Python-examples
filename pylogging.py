e775b85866461696af35fb548f08ad33d6ca8d50
rhino007US <rhino007_us@yahoo.com>
2021-07-30 15:01:41 -0500

    update

--- pylogging.py
+++ pylogging.py
@@ -1,5 +1,13 @@
 # python's Logging
 import logging
+import logging.config
+import traceback
+logging.config.fileConfig('logging.conf')
+# logging.config.dictConfig('logging.conf')
+# logger = logging.getLogger(__name__)
+logger = logging.getLogger('simpleExample')
+logger.debug('this is a debug message')
+
 # logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', datefmt='%m/%d/%Y %H:%M:%S')
 # logging.debug('this is a debug message')
 # logging.info('this is a info message')
@@ -16,7 +24,7 @@ file_h = logging.FileHandler('file.out')
 
 # level and the format
 stream_h.setLevel(logging.WARNING)
-flile_h.setLevel(logging.ERROR)
+file_h.setLevel(logging.ERROR)
 
 formatter = logging.Formatter('%(name)s - %(levelname)s - %(message)s')
 stream_h.setFormatter(formatter)
@@ -28,3 +36,29 @@ logger.addHandler(file_h)
 logger.warning('this is a warning')
 logger.error('thei is an error')
 
+try:
+    a=[1,2,3]
+    val = a[4]
+# except IndexError as e:
+#    logging.error(e, exc_info=True)
+
+# except:   will show any kind of error, not just IndexError as above calling traceback
+except:
+    logging.error("The error is %s", traceback.format_exc())
+
+from logging.handlers import RotatingFileHandler
+# import time
+# from logging.handlers import TimeRotatingFileHandler
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+# roll over after 2KB, and keep backup logs app.log.1, app.log.2 etc.
+handler = RotatingFileHandler('app.log', maxBytes=2000, backupCount=5)
+# s, m,  h, d, midnight, w0 (monday)
+# handler = TimeRotatingFileHandler('timed_test.log', when='s', interval=5 backupCount=5)
+logger.addHandler(handler)
+
+for _ in range(10000):
+    logger.info("Hello World")
+#   time.sleep(5)
